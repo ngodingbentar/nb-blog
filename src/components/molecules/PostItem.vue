999fd6f60e2ecf518183d647318aaff9683bc936
ngodingbentar <dewaqintoro@gmail.com>
2023-11-20 22:48:35 +0700

    added: primeicon

--- src/components/molecules/PostItem.vue
+++ src/components/molecules/PostItem.vue
@@ -10,13 +10,27 @@
       {{ post.name }}
     </div>
     <div class="w-2/12 px-1 justify-end items-end flex">
-      <button @click="show = !show" class="cursor-pointer text-end justify-end ">x</button>
+      <button class="btn">
+        <i class="pi pi-pencil" style="font-size: 1rem; color: blue;"></i>
+      </button>
+      <button class="ml-4 btn">
+        <i class="pi pi-trash" style="font-size: 1rem; color: red;"></i>
+      </button>
+      <!-- <button @click="show = !show" class="cursor-pointer text-end justify-end ">
+        <i class="pi pi-bars" style="font-size: 1rem"></i>
+      </button>
       <div class="relative">
         <div v-if="show" class="options absolute bg-white shadow-lg p-4 rounded-md right-0 mt-1 z-10">
-          <button class="mb-1 pb-1 border-b-2">Delete</button>
-          <button>Update</button>
+          <button class="mb-1 pb-1 border-b-2">
+            <i class="pi pi-trash" style="font-size: 1rem"></i>
+            <span class="ml-2">Update</span>
+          </button>
+          <button class="flex justify-center m-auto text-center items-center">
+            <i class="pi pi-pencil" style="font-size: 1rem"></i>
+            <span class="ml-2">Update</span>
+          </button>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
@@ -45,5 +59,9 @@ const show = ref(false)
 
 .post__item {
   @apply flex p-2 mx-2 my-1 rounded-lg border;
+}
+
+.btn {
+  @apply bg-gray-100 p-2 rounded-lg shadow-md;
 }
 </style>
