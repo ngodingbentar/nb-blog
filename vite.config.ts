2ccded8d3dfcbe2d855e27b1afa17b386dc1e899
ngodingbentar <dewaqintoro@gmail.com>
2023-11-21 19:26:51 +0700

    added: setup PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from "vite-plugin-pwa";
 
 const env = loadEnv(
   'all',
@@ -12,6 +13,36 @@ const env = loadEnv(
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'prompt',
+      injectRegister: 'script',
+      devOptions: { enabled: true },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Ngodingbentar PWA',
+        short_name: 'PWA',
+        description: 'PWA version of Ngodingbentar',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
