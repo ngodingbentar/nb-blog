b6286fe4a35e15e5b7a3af2bf46ee9e8ff222f0f
ngodingbentar <dewaqintoro@gmail.com>
2023-11-22 23:58:19 +0700

    updated: unit test

--- src/components/__tests__/CardLogin.spec.ts
+++ src/components/__tests__/CardLogin.spec.ts
@@ -1,6 +1,7 @@
-import { describe, it, expect, beforeEach, vi } from 'vitest'
 import { nextTick } from "vue";
+import { describe, it, expect, beforeEach, vi, test, afterEach } from 'vitest'
 import { setActivePinia, createPinia } from 'pinia'
+import axios from 'axios'
 
 import { mount } from '@vue/test-utils'
 import CardLogin from '@/components/organisms/CardLogin.vue'
@@ -8,12 +9,22 @@ import InputMolecule from '@/components/molecules/InputMolecule.vue'
 import ButtonAtom from '@/components/atoms/ButtonAtom.vue'
 import HeaderMolecule from '@/components/molecules/HeaderMolecule.vue'
 
+import { doLogin, doRegister } from '@/api/users'
+import { useMainStore } from '@/stores/main'
+
 vi.mock('vue-router')
+vi.mock('axios')
 
 describe('CardLogin', () => {
   beforeEach(() => {
     setActivePinia(createPinia())
+    vi.useFakeTimers({ shouldAdvanceTime: true });
   })
+
+  afterEach(() => {
+    vi.runOnlyPendingTimers();
+    vi.useRealTimers();
+  });
 
   it('renders properly', async () => {
     const wrapper = mount({
@@ -44,5 +55,70 @@ describe('CardLogin', () => {
     // console.log('wrapper.html()', wrapper.html())
     await nextTick()
     wrapper.find('button').trigger('click')
+  })
+
+  test('Login', async () => {
+    const wrapper = mount({
+      props: {
+        mode: '',
+      },
+      data: () => {
+        return {
+          error: '',
+        }
+      },
+      components: {
+        CardLogin,
+        InputMolecule,
+        ButtonAtom,
+        HeaderMolecule,
+      },
+      template: `
+      <CardLogin>
+        <div class="login__card">
+          <div class="login__form">
+            <HeaderMolecule :title="mode === 'login' ? 'Sign in to your account' : 'Register for free'" :error="error" />
+            <form class="space-y-4 md:space-y-6" action="#">
+              <InputMolecule label="Email" type="email" v-model="email" placeholder="email@example.com" />
+              <InputMolecule label="Password" type="password" v-model="password" placeholder="••••••••" />
+              <ButtonAtom @click="submit" :label="mode === 'login' ? 'Login' : 'Register'" />
+            </form>
+            <div>
+              <p class="text-sm font-light">
+                {{ mode === 'login' ? 'Don’t have an account yet?' : 'Already have an account?' }}
+                <RouterLink :to="mode === 'login' ? '/register' : '/login'">
+                  <span class="font-medium text-primary hover:underline cursor-pointer">
+                    {{ mode === 'login' ? 'Register' : 'Login' }}
+                  </span>
+                </RouterLink>
+              </p>
+            </div>
+          </div>
+        </div>
+      </CardLogin>
+      `,
+    })
+  
+    const newUserMock = {
+      token: 'token1234'
+    }
+  
+    vi.mocked(axios.post).mockResolvedValue({
+      data: newUserMock,
+    })
+    
+    wrapper.setProps({
+      mode: 'login',
+    })
+    await nextTick()
+    wrapper.find('.btn__login').trigger('click')
+    wrapper.setData({
+      error: 'Something went wrong',
+    })
+    vi.advanceTimersByTime(3000)
+    wrapper.setData({
+      error: '',
+    })
+    expect(wrapper.vm.mode).toBe('login')
   })
 })
--- src/components/__tests__/MainStore.spec.ts
+++ src/components/__tests__/MainStore.spec.ts
@@ -1,11 +1,18 @@
-import { describe, expect, beforeEach, test } from 'vitest'
+import { describe, expect, beforeEach, test, vi, afterEach } from 'vitest'
 import { createPinia, setActivePinia } from 'pinia'
 import { useMainStore } from '@/stores/main'
+import { nextTick } from 'vue'
 
 describe('MainStore', () => {
   beforeEach(() => {
     setActivePinia(createPinia())
+    vi.useFakeTimers({ shouldAdvanceTime: true });
   })
+
+  afterEach(() => {
+    vi.runOnlyPendingTimers();
+    vi.useRealTimers();
+  });
 
   test('initial state', async () => {
     const mainStore = useMainStore()
@@ -40,5 +47,29 @@ describe('MainStore', () => {
     expect(mainStore.toastStore.type).toBe('success')
     expect(mainStore.toastStore.title).toBe('Success')
     expect(mainStore.toastStore.content).toBe('Success add data')
+  })
+
+  test('resetToast', async () => {
+    const mainStore = useMainStore()
+    mainStore.resetToast()
+    expect(mainStore.isToast).toBe(false)
+    expect(mainStore.toastStore.type).toBe('')
+    expect(mainStore.toastStore.title).toBe('')
+    expect(mainStore.toastStore.content).toBe('')
+  })
+
+  test('watch isToast', async () => {
+    const mainStore = useMainStore()
+    mainStore.isToast = true
+  
+    if(mainStore.isToast) {
+      mainStore.isToast = false
+      mainStore.toastStore.title = ''
+      mainStore.toastStore.content = ''
+      vi.advanceTimersByTime(3000)
+    }
+    expect(mainStore.isToast).toBe(false)
+    expect(mainStore.toastStore.title).toBe('')
+    expect(mainStore.toastStore.content).toBe('')
   })
 })
